8a4148946319b3259cfc980792982eb488969053
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2018-06-06 17:20:27 +0600

    UserRepository.java update

--- src/main/java/com/epam/adok/jpaproject/repository/UserRepository.java
+++ src/main/java/com/epam/adok/jpaproject/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.epam.adok.jpaproject.repository;
 
 import com.epam.adok.jpaproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findUserById(Long id);
